2e4f745e4f5f16d32511cffcc4dd0d183ae3a8bc
Jared Tan <83501266+jaredtcy@users.noreply.github.com>
2021-05-24 15:18:06 +0800

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,28 +1,70 @@
-<?php
+// Replace this with your own email address
+$siteOwnersEmail = 'user@website.com';
 
-if($_POST["foo"]==''):/* if the input is not empty it means that a SPAM BOT filled up the entire form */
-		
-	$name=$_POST["name"];
-	$email=$_POST["email"];
-	$message=$_POST["message"];
-	$recipient=$_POST["recipient"];
-	
-	if($name=='' || $email=='' || $message==''): echo '2'; exit(); endif; /* if some values is missing, return the error */
-	
-	/* remove this line */
-	if($recipient==''): echo '1'; exit(); endif;
-	/* end removable line */
-	
-	$date=date("d M Y - h:1 a");
 
-	$mail=$tanchaoyang93@outlook.com; /* change this variable with your email address */
-	$header = "From: ".$name." <".$email.">" . "\r\n" . "Reply-To: ".$email."" . "\r\n" . "X-Mailer: PHP/" . phpversion();	
-    $header .= "MIME-Version: 1.0\n";
-	$header .= "Content-Type: text/html; charset=\"iso-8859-1\"\n";
-	$header .= "Content-Transfer-Encoding: 7bit\n\n";
-	$msg= '<html><body>New message from Your Website Contact Page.<br /><br /><strong>Name:</strong> '.$name.'<br /><br /><strong>Email:</strong> '.$email.'<br /><br /><strong>Message:</strong> '.nl2br($message).'<br /><br /><strong>Date:</strong> '.$date.'<br /><br /><br /><small><i>From - Your website</i></small></body></html>';
-	if(@mail("$mail","Your website - Contact form",$msg,$header)): echo '1'; else: echo '3'; endif;/* check if the mail() function succeded */
-	
-else: echo '1'; endif;/* send a positive fake return if SPAM is detected */
+if($_POST) {
+
+    $name = trim(stripslashes($_POST['contactName']));
+    $email = trim(stripslashes($_POST['contactEmail']));
+    $subject = trim(stripslashes($_POST['contactSubject']));
+    $contact_message = trim(stripslashes($_POST['contactMessage']));
+
+    // Check Name
+    if (strlen($name) < 2) {
+        $error['name'] = "Please enter your name.";
+    }
+    // Check Email
+    if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+        $error['email'] = "Please enter a valid email address.";
+    }
+    // Check Message
+    if (strlen($contact_message) < 15) {
+        $error['message'] = "Please enter your message. It should have at least 15 characters.";
+    }
+    // Subject
+    if ($subject == '') { $subject = "Contact Form Submission"; }
+
+
+    // Set Message
+    $message .= "Email from: " . $name . "<br />";
+    $message .= "Email address: " . $email . "<br />";
+    $message .= "Message: <br />";
+    $message .= $contact_message;
+    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+
+    // Set From: header
+    $from =  $name . " <" . $email . ">";
+
+    // Email Headers
+    $headers = "From: " . $from . "\r\n";
+    $headers .= "Reply-To: ". $email . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+    if (!$error) {
+
+        ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+        $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+        if ($mail) { echo "OK"; }
+        else { echo "Success! Thanks!"; }
+        
+    } # end if - no validation error
+
+    else {
+
+        $response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+        $response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+        $response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+        
+        echo $response;
+
+    } # end if - there was a validation error
+
+}
 
 ?>
+
+
+
